ac565854a5a0bc4c9295686552921d9dbfaa32f0
vikikimart <vikiki.mart@gmail.com>
2019-10-22 23:17:27 +0300

    Full home work1

---
+++ HomeWork1/TASK2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c, d; //здесь объявляются переменные, а- ширина ПП, b- высота и с - длина
+            a = 2; //здесь ширине а присваивается значение 2 
+            b = 3; //здесь высоте b присваивается значение 3
+            c = 4; // здесь длине c присваивается значение 4
+            d = Math.Sqrt(a * a + b * b + c * c); //здесь вычисляется формула длина диагонали ПП
+            Console.WriteLine($"a={a}");
+            Console.WriteLine($"b={b}");
+            Console.WriteLine($"c={c}");
+            Console.WriteLine($"Parallelepiped diagonal length: {d}");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ HomeWork1/TASK4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            string s1 = "hello";
+            string s2 = "world";
+            string s3 = s1 + " " + s2; // результат: строка "hello world"
+            string s4 = String.Concat(s3, "!!!"); // результат: строка "hello world!!!"
+
+            Console.WriteLine(s4);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
